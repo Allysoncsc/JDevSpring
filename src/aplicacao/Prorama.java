94c62c20f0d3d67d877e98aa50c072685addec18
Allysoncs <allysoncsc@gmail.com>
2024-06-18 23:04:50 -0300

    Começo do projeto, criando classes e main

---
+++ src/aplicacao/Prorama.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Prorama {
+
+    public static void main(String[] args){
+        Pessoa p1 = new Pessoa(1,"Allyson Correia","allyson@gmail.com");
+        Pessoa p2 = new Pessoa(2,"Alailson Correa","alailson@gmail.com");
+        Pessoa p3 = new Pessoa(3,"Alais Correia","alais@gmail.com");
+    }
+}
